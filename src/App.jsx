1d59a6636cb19931ad776e3eb9eda86eacd8cedc
Phils9 <philip.thirdangle@gmail.com>
2025-03-30 17:45:48 +0100

    Uninstalled older version of tailwindcss@3

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
 import Dashboard from "./components/Dashboard";
 import Footer from "./components/Footer";
+import ErrorMessage from "./components/ErrorMessage";
 
 
 function App() {
   const [city, setCity] = useState("");
+  const [error, setErro] = useState('');
 
+ 
   return (
     <div className="min-h-screen flex flex-col bg-[url('https://i.ibb.co/93kHk2cx/My-Project-1.png')] bg-cover bg-center">
       <Header />
       <main className="flex-1">
         <SearchBar onSearch={setCity} />
+        <ErrorMessage message={error} />
         <Dashboard city={city}/>
       </main>
       <Footer />
---
+++ src/components/ErrorMessage.jsx
@@ -0,0 +1,12 @@
+export default function ErrorMessage({ message }) {
+  if (!message) return null;
+
+  return (
+    <div className="bg-yellow-50 border-l-4 border-yellow-400 p-4 my-4 rounded-lg">
+      <div className="flex items-center">
+        <span className="text-yellow-700">⚠️</span>
+        <p className="ml-2 text-yellow-700">{message}</p>
+      </div>
+    </div>
+  );
+}
